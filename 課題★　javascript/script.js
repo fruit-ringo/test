743573deba161a972c960ba0565e5d33b7297ff9
fruit-ringo <aguri1003100@yahoo.co.jp>
2018-11-04 17:43:07 +0900

    コメント

---
+++ 課題★　javascript/script.js
@@ -0,0 +1,33 @@
+for(var a=1;a<=1;a++){document.write("<br>問１<br>")}　
+
+for(var b=1;b<=5;b++){document.write("★");}
+
+for(var c=1;c<=1;c++){document.write("<br>問２<br>");}
+
+for(var i=1;i<=3;i++){for(var a=1;a<=3;a++){document.write("★");}
+                      document.write("<br>");}
+
+for(var a=1;a<=1;a++){document.write("<br>問3<br>");}
+
+for(var a=1;a<=2;a++){for(var b=1;b<=5;b++){document.write("☆");}document.write("<br>");}
+
+for(var a=1;a<=1;a++){document.write("<br>問４<br>");}
+
+for(var a=1;a<=4;a++){for(var b=1;b<=5;b++){document.write("★");}document.write("<br>");}
+
+for(var a=1;a<=1;a++){document.write("<br>問５<br>");}
+
+for(var a=1;a<=4;a++){for(var b=1;b<=3;b++){document.write("★");}document.write("<br>");}
+                     
+for(var a=1;a<=1;a++){document.write("<br>問６<br>");}
+
+for(var a=1;a<=3;a++){for(var b=1;b<=3;b++){if(b%2==0){document.write("☆");}else{document.write("★");}}document.write("<br>");}
+
+for(var a=1;a<=1;a++){document.write("<br>問７<br>");}
+
+for(var a=1;a<=4;a++){for(var b=1;b<=5;b++){if(b%2==0){document.write("☆");}else{document.write("★");}}document.write("<br>");}
+
+for(var a=1;a<=1;a++){document.write("<br>問８<br>");}
+
+
+for(i=0;i<=4;i++) for(j=0;j<=4;j++){if(i==j){document.write("★<br>");break;}else{document.write("★");}}
